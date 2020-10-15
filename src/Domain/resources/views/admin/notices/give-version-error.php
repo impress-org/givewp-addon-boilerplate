388d01123609d999adaeef3ee8ddff338d7a9e1e
Devin Walker <sdtucson@gmail.com>
2020-10-15 13:46:18 -0700

    fix: text should be "GiveWP" in notice, not "Give"

--- src/Domain/resources/views/admin/notices/give-version-error.php
+++ src/Domain/resources/views/admin/notices/give-version-error.php
@@ -3,7 +3,7 @@
 <strong>
 	<?php _e( 'Activation Error:', 'ADDON_TEXTDOMAIN' ); ?>
 </strong>
-<?php _e( 'You must have', 'ADDON_TEXTDOMAIN' ); ?> <a href="https://givewp.com" target="_blank">Give</a>
+<?php _e( 'You must have', 'ADDON_TEXTDOMAIN' ); ?> <a href="https://givewp.com" target="_blank">GiveWP</a>
 <?php _e( 'version', 'ADDON_TEXTDOMAIN' ); ?> <?php echo GIVE_VERSION; ?>+
 <?php printf( esc_html__( 'for the %1$s add-on to activate', 'ADDON_TEXTDOMAIN' ), ADDON_CONSTANT_NAME ); ?>.
 
