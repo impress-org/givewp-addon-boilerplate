381e8ff656a48ee8a300df4ba52b643855befb3f
Devin Walker <sdtucson@gmail.com>
2021-06-16 13:50:36 -0700

    fix: typo in notice

--- src/Domain/resources/views/admin/notices/give-inactive.php
+++ src/Domain/resources/views/admin/notices/give-inactive.php
@@ -3,7 +3,7 @@
 <div class="notice notice-error">
 	<p>
 		<strong><?php _e( 'Activation Error:', 'ADDON_TEXTDOMAIN' ); ?></strong>
-		<?php _e( 'You must have', 'ADDON_TEXTDOMAIN' ); ?> <a href="https://givewp.com" target="_blank">GiveWP</a>
+		<?php _e( 'You must have the', 'ADDON_TEXTDOMAIN' ); ?> <a href="https://givewp.com" target="_blank">GiveWP</a>
 		<?php printf( __( 'plugin installed and activated for the %s add-on to activate', 'ADDON_TEXTDOMAIN' ), ADDON_CONSTANT_NAME ); ?>
 	</p>
 </div>
