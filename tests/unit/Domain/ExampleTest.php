b4528ddf6ea6f0f9bfb1fd4e87af0dbae01f830e
Kyle B. Johnson <kyle.johnson@impress.org>
2021-11-18 13:11:11 -0500

    wip: Setup unit tests.

---
+++ tests/unit/Domain/ExampleTest.php
@@ -0,0 +1,8 @@
+<?php
+
+class ExampleTest extends Give_Unit_Test_Case {
+
+    public function test_true() {
+        $this->assertTrue( false );
+    }
+}
---
+++ tests/unit/bootstrap.php
@@ -0,0 +1,134 @@
+<?php
+
+/**
+ * Give Unit Tests Bootstrap
+ *
+ * @unreleased
+ */
+class Give_GiveAddon_Unit_Tests_Bootstrap {
+
+	/** @var \Give_Unit_Tests_Bootstrap instance */
+	protected static $instance = NULL;
+
+	/** @var string directory where wordpress-tests-lib    is installed */
+	public $wp_tests_dir;
+
+	/** @var string testing directory */
+	public $tests_dir;
+
+	/** @var string plugin directory */
+	public $plugin_dir;
+
+	/** @var string give directory */
+	public $give_dir;
+
+	/**
+	 * Give_RD_Unit_Tests_Bootstrap constructor.
+	 *
+	 * Setup the unit testing environment
+	 */
+	public function __construct() {
+
+		ini_set( 'display_errors', 'on' );
+		error_reporting( E_ALL );
+
+		// Prevent give_die() from stopping tests.
+		if ( ! defined( 'GIVE_UNIT_TESTS' ) ) {
+			define( 'GIVE_UNIT_TESTS', TRUE );
+		}
+
+		$this->tests_dir    = dirname( __FILE__ );
+		$this->plugin_dir   = dirname( $this->tests_dir );
+		$this->give_dir     = dirname( dirname( $this->plugin_dir ) ) . '/give';
+		$this->wp_tests_dir = getenv( 'WP_TESTS_DIR' ) ? getenv( 'WP_TESTS_DIR' ) : '/tmp/wordpress-tests-lib';
+
+		// Load test function so tests_add_filter() is available
+		require_once( "{$this->wp_tests_dir}/includes/functions.php" );
+
+		// Run bootstrap.php in this file only.
+		$GLOBALS['manual_bootstrap'] = FALSE;
+
+		// Load Give
+        if( file_exists( "{$this->give_dir}/tests/unit/bootstrap.php" ) ) {
+            require_once("{$this->give_dir}/tests/unit/bootstrap.php");
+        } else {
+            require_once("{$this->wp_tests_dir}/give/tests/unit/bootstrap.php");
+        }
+
+		// Uninstall plugin.
+		tests_add_filter( 'plugins_loaded', array( $this, 'uninstall_plugin' ), 0 );
+
+		// Load plugin
+		tests_add_filter( 'muplugins_loaded', array( $this, 'load_plugin' ) );
+
+
+		// Install plugin
+		tests_add_filter( 'init', array( $this, 'install_plugin' ) );
+
+		// Load the WP testing environment
+		require_once( "{$this->wp_tests_dir}/includes/bootstrap.php" );
+
+		// Load Give testing framework
+		$this->includes();
+	}
+
+	/**
+	 * Load recurring itself.
+	 */
+	public function load_plugin() {
+		require_once( dirname($this->plugin_dir) . "/ADDON_ID.php" );
+	}
+
+	/**
+	 * Uninstall plugin
+	 */
+	public function uninstall_plugin() {
+		give_update_option( 'uninstall_on_delete', 'enabled' );
+
+		// WP_UNINSTALL_PLUGIN is already define while uninstalling give.
+//		include( $this->plugin_dir . '/uninstall.php' );
+	}
+
+	/**
+	 * Install recurring.
+	 */
+	public function install_plugin() {
+
+		echo 'Installing ADDON_NAME...' . PHP_EOL;
+
+		// give_{addon}_install();
+	}
+
+	/**
+	 * Load Give-specific test cases
+	 *
+	 * @since 1.4
+	 */
+	public function includes() {
+		Give_Unit_Tests_Bootstrap::instance()->includes();
+
+		require_once( $this->tests_dir . '/helpers.php' );
+
+		// test cases
+		// require_once( $this->tests_dir . '/framework/class-{testcase}.php' );
+
+		//Helpers
+		// require_once( $this->tests_dir . '/framework/helpers/class-{helper}.php' );
+	}
+
+	/**
+	 * Get the single class instance.
+	 *
+	 * @since 1.4
+	 * @return Give_Unit_Tests_Bootstrap
+	 */
+	public static function instance() {
+		if ( is_null( self::$instance ) ) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+}
+
+Give_GiveAddon_Unit_Tests_Bootstrap::instance();
