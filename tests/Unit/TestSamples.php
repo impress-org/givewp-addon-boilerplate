ed78f61674204b9308c525b200a32b650517a01e
Glauber Silva <glauber.silva@givewp.com>
2023-04-26 14:35:53 -0300

    refactor: update files and add support for tests and ts

---
+++ tests/Unit/TestSamples.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace GivePdfReceipt\Tests\PdfExport\EventHandlers;
+
+use Exception;
+use Give\Tests\TestCase;
+use Give\Tests\TestTraits\RefreshDatabase;
+use WPDieException;
+
+class TestSamples extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @unreleased
+     *
+     * @throws Exception
+     */
+    public function testExpectException()
+    {
+        $this->expectException(WPDieException::class);
+
+        wp_die();
+    }
+
+    /**
+     * @unreleased
+     */
+    public function testAssertTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * @unreleased
+     */
+    public function testAssertEquals()
+    {
+        $this->assertEquals(true, true);
+    }
+}
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,24 +1,49 @@
-const mix = require( 'laravel-mix' );
-const wpPot = require( 'wp-pot' );
+const mix = require('laravel-mix');
+const wpPot = require('wp-pot');
 
-mix
-	.setPublicPath( 'public' )
-	.sourceMaps( false )
+mix.setPublicPath('public')
+    .sourceMaps(false)
 
-	// admin assets
-	.js( 'src/Domain/resources/js/admin/ADDON_ID-admin.js', 'public/js/' )
-	.sass( 'src/Domain/resources/css/admin/ADDON_ID-admin.scss', 'public/css' )
+    // admin assets
+    .ts('src/Domain/resources/js/admin/ADDON_ID-admin.tsx', 'public/js/')
+    .sass('src/Domain/resources/css/admin/ADDON_ID-admin.scss', 'public/css')
 
-	// public assets
-	.js( 'src/Domain/resources/js/frontend/ADDON_ID.js', 'public/js/' )
-	.sass( 'src/Domain/resources/css/frontend/ADDON_ID-frontend.scss', 'public/css' )
+    // public assets
+    .js('src/Domain/resources/js/frontend/ADDON_ID.js', 'public/js/')
+    .sass('src/Domain/resources/css/frontend/ADDON_ID-frontend.scss', 'public/css')
 
-	// images
-	.copy( 'src/Domain/resources/images/*.{jpg,jpeg,png,gif}', 'public/images' );
+    // images
+    .copy('src/Domain/resources/images/*.{jpg,jpeg,png,gif,svg}', 'public/images');
 
-mix.webpackConfig( {
-	externals: {
-		$: 'jQuery',
-		jquery: 'jQuery',
-	},
-} );
+mix.webpackConfig({
+    externals: {
+        $: 'jQuery',
+        jquery: 'jQuery',
+    },
+});
+
+mix.options({
+    // Don't perform any css url rewriting by default
+    processCssUrls: false,
+
+    // Prevent LICENSE files from showing up in JS builds
+    terser: {
+        extractComments: (astNode, comment) => false,
+        terserOptions: {
+            format: {
+                comments: false,
+            },
+        },
+    },
+});
+
+if (mix.inProduction()) {
+    wpPot({
+        package: 'ADDON_NAME',
+        domain: 'ADDON_TEXTDOMAIN',
+        destFile: 'languages/ADDON_TEXTDOMAIN.pot',
+        relativeTo: './',
+        bugReport: 'https://github.com/impress-org/give/issues/new/choose',
+        team: 'GiveWP <info@givewp.com>',
+    });
+}
