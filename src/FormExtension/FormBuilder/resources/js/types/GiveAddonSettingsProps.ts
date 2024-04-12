a9ea6263fbd29fead0a2371b32485353b65d162d
Glauber Silva <glauber.silva@givewp.com>
2024-04-12 17:17:35 -0300

    refactor: rename and remove old strings

--- src/FormExtension/FormBuilder/resources/js/types/GiveAddonSettingsProps.ts
+++ src/FormExtension/FormBuilder/resources/js/types/GiveAddonSettingsProps.ts
@@ -10,23 +10,4 @@ export type GiveAddonSettingsProps = {
     textareaOptionSample: string;
     classicEditorOptionSample: string;
     imageSelectorOptionSample: string;
-
-    generationMethod: string;
-    colorPicker: string;
-    templateId: string;
-    logoUpload: string;
-    companyName: string;
-    name: string;
-    addressLine1: string;
-    addressLine2: string;
-    cityStateZip: string;
-    displayWebsiteUrl: boolean;
-    emailAddress: string;
-    headerMessage: string;
-    footerMessage: string;
-    additionalNotes: string;
-    customTemplateId: string;
-    customTemplateName: string;
-    customPageSize: string;
-    customPdfBuilder: string;
 };
