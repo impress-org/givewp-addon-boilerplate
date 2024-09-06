fa57fda0b32c9165c92d7c6d6c26fa1f5cc8a69d
Glauber Silva <glauber.silva@givewp.com>
2024-09-06 16:05:22 -0300

    refactor: rename logo

--- src/OffSiteGateway/Gateway/resources/OffSiteGatewayLogo.tsx
+++ src/OffSiteGateway/Gateway/resources/OffSiteGatewayLogo.tsx
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
 
-export default function GiveAddonLogo() {
+export default function GiveAddonOffSiteGatewayLogo() {
     return <img src={logo} alt="ADDON_NAME - OffSite Gateway Logo" />;
 }
